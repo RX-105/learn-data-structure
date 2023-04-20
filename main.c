0fee05c55e9d1b7e29fb340065396fde4da9c6a7
n0sense <1436648676@qq.com>
2023-04-20 19:25:10 +0800

    字符串暴力匹配法测试

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
-#include "SqList.h"
-#include "LinkList.h"
+#include "chapter2/SqList.h"
+#include "chapter2/LinkList.h"
+#include "chapter4/SString.h"
 #include "stdio.h"
+#include "string.h"
 
 int main()
 {
     // Initialization
-    LinkList list;
-    list = LinkListHeadInit(list, 10);
+    SString master;
+    SString pattern;
+    StrAssign(&master, "ababcabcacbab", 13);
+    StrAssign(&pattern, "abcac", 5);
 
     // Operations
+    printf("Index at: %d\n", Index(&master, &pattern));
 
     // Output
-    LinkListPrint(list);
     return 0;
 }
