0a0de50338c53995152231256223c279cc89317a
n0sense <1436648676@qq.com>
2023-04-27 20:58:46 +0800

    测试用例

--- main.c
+++ main.c
@@ -10,18 +10,15 @@ int main() {
     LinkList l = NULL;
     LinkList l2 = NULL;
     int arr[6] = {1,2,3,4,5,6};
-    int arr2[3] = {2,2,3};
+    int arr2[3] = {4,5,6};
     l = LinkListTailInit(l, 6, arr);
     l2 = LinkListTailInit(l2, 3, arr2);
 
     // Operations
-    // 手动构造一个含环的链表
-    l2->next->next->next->next = l2->next->next;
-    int res = ContainsRing(l2);
+    LinkListRearrange(l);
 
     // Output
     LinkListPrint(l);
-    printf("Contains ring: %d\n", res);
 
     return 0;
 }
