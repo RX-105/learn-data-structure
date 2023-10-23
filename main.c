7f2d15234a186ebc5dedf0c8e844e80013ecb755
n0sense <1436648676@qq.com>
2023-10-23 15:09:28 +0800

    模拟题第一、二题测试代码

--- main.c
+++ main.c
@@ -3,7 +3,36 @@
 #include "chapter3/SqStack.h"
 #include "chapter4/SString.h"
 #include "stdio.h"
-#include "string.h"
+
+int sqlist_insert(SqList *list, int index, int data){
+    int tmp, i = index;
+    if (index > list->length){
+        return -1;
+    }
+    else{
+        while (i < list->length){
+            tmp = list->data[i];
+            list->data[i] = data;
+            i++;
+            data = tmp;
+        }
+        return 0;
+    }
+}
+
+int sqlist_delete_first(SqList *list) {
+    if (list->length == 0) {
+        return -1;
+    } else {
+        int i = 0;
+        while (i < list->length) {
+            list->data[i] = list->data[i];
+            i++;
+        }
+        list->length--;
+        return 0;
+    }
+}
 
 int main() {
     // Initialization
@@ -11,14 +40,19 @@ int main() {
     LinkList l2 = NULL;
     int arr[6] = {1,2,3,4,5,6};
     int arr2[3] = {4,5,6};
-    l = LinkListTailInit(l, 6, arr);
-    l2 = LinkListTailInit(l2, 3, arr2);
+    SqList l3;
+    SqListInit(&l3, 7);
+    SqListFromArray(&l3, arr, 6);
 
     // Operations
-    LinkListRearrange(l);
+//    LinkListRearrange(l);
+    SqListPrint(&l3);
+    sqlist_insert(&l3, 1, 10);
+    sqlist_delete_first(&l3);
+    SqListPrint(&l3);
 
     // Output
-    LinkListPrint(l);
+//    LinkListPrint(l);
 
     return 0;
 }
